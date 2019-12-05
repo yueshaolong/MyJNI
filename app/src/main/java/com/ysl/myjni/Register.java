9a9e86ad00f6d498020fe897042bd7de8f2205b0
dragon <809629457@qq.com>
2019-12-05 17:19:09 +0800

    Initial commit

---
+++ app/src/main/java/com/ysl/myjni/Register.java
@@ -0,0 +1,22 @@
+package com.ysl.myjni;
+
+public class Register {
+
+    static {
+        System.load("C:\\Work\\CLionProjects\\JNIRegister\\cmake-build-debug" +
+                "\\libHello.dll");
+        System.load("C:\\Work\\CLionProjects\\JNIRegister\\cmake-build-debug" +
+                "\\libDyHello.dll");
+    }
+
+    public native String helloworld();
+    public native String dyHello();
+
+    public static void main(String[] args) {
+        Register register = new Register();
+        String helloworld = register.helloworld();
+        System.out.println(helloworld);
+        String dyHello = register.dyHello();
+        System.out.println(dyHello);
+    }
+}
